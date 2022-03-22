vproc/vicuna

commit 0e48ed1147fba0aac8d605e0673f82463b041cda

Remove obsolete flags for pending loads and stores
Since switching to dedicated pending load and store flags for the
register unpacking in the LSU, there is no more need for separately
tracking whether any or all valid states have a certain set of flags
set or cleared.

rtl/vproc_pipeline.sv
@@ -713,15 +713,15 @@ module vproc_pipeline #(
     ///////////////////////////////////////////////////////////////////////////
     // LSU PENDING LOADS AND STORES SIGNALS
 
-    ctrl_t unpack_flags_all, unpack_flags_any;
+    ctrl_t unpack_flags;
     logic  lsu_pending_load, lsu_pending_store;
     assign pending_load_o  = UNITS[UNIT_LSU] & (
                                  (state_valid_q & (state_q.unit == UNIT_LSU) & ~state_q.mode.lsu.store) |
-                                 unpack_flags_all.pend_load  | lsu_pending_load
+                                 unpack_flags.pend_load  | lsu_pending_load
                              );
     assign pending_store_o = UNITS[UNIT_LSU] & (
                                  (state_valid_q & (state_q.unit == UNIT_LSU) &  state_q.mode.lsu.store) |
-                                 unpack_flags_any.pend_store | lsu_pending_store
+                                 unpack_flags.pend_store | lsu_pending_store
                              );
 
 
@@ -778,8 +778,8 @@ module vproc_pipeline #(
         .pipe_out_op_data_o   ( unpack_out_ops               ),
         .pending_vreg_reads_o ( unpack_pend_rd               ),
         .stage_valid_any_o    (                              ),
-        .ctrl_flags_any_o     ( unpack_flags_any             ),
-        .ctrl_flags_all_o     ( unpack_flags_all             )
+        .ctrl_flags_any_o     ( unpack_flags                 ),
+        .ctrl_flags_all_o     (                              )
     );
 
     assign op_addr_offset_pend_reads_clear = unpack_out_valid & unpack_out_ctrl.last_cycle;

commit 3bf8cdbc571be71e07ba12ec1094de8d3f3daf84

Fix syntax error introduced in 0e48ed1
Fix the syntax error resulting from a name collision introduced in
commit 0e48ed1.

rtl/vproc_pipeline.sv
@@ -713,15 +713,15 @@ module vproc_pipeline #(
     ///////////////////////////////////////////////////////////////////////////
     // LSU PENDING LOADS AND STORES SIGNALS
 
-    ctrl_t unpack_flags;
+    ctrl_t unpack_ctrl_flags;
     logic  lsu_pending_load, lsu_pending_store;
     assign pending_load_o  = UNITS[UNIT_LSU] & (
                                  (state_valid_q & (state_q.unit == UNIT_LSU) & ~state_q.mode.lsu.store) |
-                                 unpack_flags.pend_load  | lsu_pending_load
+                                 unpack_ctrl_flags.pend_load  | lsu_pending_load
                              );
     assign pending_store_o = UNITS[UNIT_LSU] & (
                                  (state_valid_q & (state_q.unit == UNIT_LSU) &  state_q.mode.lsu.store) |
-                                 unpack_flags.pend_store | lsu_pending_store
+                                 unpack_ctrl_flags.pend_store | lsu_pending_store
                              );
 
 
@@ -778,7 +778,7 @@ module vproc_pipeline #(
         .pipe_out_op_data_o   ( unpack_out_ops               ),
         .pending_vreg_reads_o ( unpack_pend_rd               ),
         .stage_valid_any_o    (                              ),
-        .ctrl_flags_any_o     ( unpack_flags                 ),
+        .ctrl_flags_any_o     ( unpack_ctrl_flags            ),
         .ctrl_flags_all_o     (                              )
     );
 