e40ebd29104d03a0e1515636aedeb81d69cb8fa2
Moritz Imfeld <moritz.imfeld@csem.ch>
2022-03-23 09:08:50 +0100

    Add questa as a simulator option
     - Add sim_questa.tcl file to compile and run the simulation
     - Add questa target to the sim/Makefile
     - Add questa as an option to the README"
    
    Signed-off-by: Moritz Imfeld <moritz.imfeld@csem.ch>

---
+++ sim/sim_questa.tcl
@@ -0,0 +1,118 @@
+# Copyright 2022 CSEM
+#
+# This file, and derivatives thereof are licensed under the
+# Solderpad License, Version 2.0 (the "License");
+# Use of this file means you agree to the terms and conditions
+# of the license and are in full compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://solderpad.org/licenses/SHL-2.0/
+#
+# Unless required by applicable law or agreed to in writing, software
+# and hardware implementations thereof
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+if {$argc < 5} {
+    puts "usage: sim.tcl VPROC_DIR CORE_DIR PARAMS LOG_FILE PROG_PATHS_LIST \[SYMBOLS ...\]"
+    exit 2
+}
+
+# get command line arguments
+set vproc_dir $1
+set core_dir $2
+set params_var $3
+set log_file_path $4
+set prog_paths_var $5
+set log_signals $6
+
+# default values for dv_utils and prim
+set dv_utils ""
+set prim ""
+
+lassign $params_var VREG_W VMEM_W VMUL_W VALU_W VSLD_W ICACHE_SZ ICACHE_LINE_W DCACHE_SZ DCACHE_LINE_W MEM_W MEM_SZ MEM_LATENCY
+
+# RAM_TYPE=2 is translated to vproc_pkg::RAM_ASIC
+set RAM_TYPE "2"
+# MUL_TYPE=0 is translated to vproc_pkg::MUL_GENERIC
+set MUL_TYPE "0"
+
+# create source file list
+lappend src_list "$vproc_dir/rtl/vproc_pkg.sv"
+lappend src_list "$vproc_dir/sim/vproc_tb.sv"
+set main_core ""
+if {[string first "ibex" $core_dir] != -1} {
+    set main_core "MAIN_CORE_IBEX"
+    lappend src_list "$core_dir/rtl/ibex_pkg.sv"
+    lappend src_list "$core_dir/rtl/ibex_tracer_pkg.sv"
+    lappend src_list "$core_dir/vendor/lowrisc_ip/ip/prim/rtl/prim_ram_1p_pkg.sv"
+    foreach file [glob -type f $core_dir\/*rtl\/*\[a-z,_\]*.sv] {
+        lappend src_list $file
+    }
+    lappend src_list "$core_dir/syn/rtl/prim_clock_gating.v"
+    set dv_utils "$core_dir/vendor/lowrisc_ip/dv/sv/dv_utils/"
+    set prim "$core_dir/vendor/lowrisc_ip/ip/prim/rtl/"
+} elseif {[string first "cv32e40x" $core_dir] != -1} {
+    set main_core "MAIN_CORE_CV32E40X"
+    lappend src_list "$core_dir/rtl/include/cv32e40x_pkg.sv"
+    foreach file [glob -type f $core_dir\/*rtl\/*\[a-z,_\]*.sv] {
+        lappend src_list $file
+    }
+    lappend src_list "$core_dir/bhv/cv32e40x_sim_clock_gate.sv"
+}
+foreach file [glob -type f $vproc_dir\/*rtl\/*\[a-z,_\]*.sv] {
+    lappend src_list $file
+}
+
+vlib work
+
+foreach file $src_list {vlog -work work $file +define+$main_core +incdir+$prim+$dv_utils}
+
+vopt +acc vproc_tb -o vproc_tb_opt -debugdb -G VREG_W=$VREG_W \
+     -G VMEM_W=$VMEM_W -G VMUL_W=$VMUL_W -G VALU_W=$VALU_W    \
+     -G VSLD_W=$VSLD_W -G ICACHE_SZ=$ICACHE_SZ                \
+     -G ICACHE_LINE_W=$ICACHE_LINE_W -G DCACHE_SZ=$DCACHE_SZ  \
+     -G DCACHE_LINE_W=$DCACHE_LINE_W -G MEM_W=$MEM_W          \
+     -G MEM_SZ=$MEM_SZ -G MEM_LATENCY=$MEM_LATENCY            \
+     -G RAM_TYPE=$RAM_TYPE -G MUl_TYPE=$MUL_TYPE              \
+     +define+$main_core -G PROG_PATHS_LIST=$prog_paths_var
+
+vsim -work work vproc_tb_opt
+
+# add trace_signals
+foreach sig $log_signals {
+    add wave $sig
+}
+
+set complete_signal "done"
+add wave "done"
+
+set outf [open $log_file_path "w"]
+puts "logging following signals to $log_file_path: $log_signals"
+
+foreach sig $log_signals {
+    set sig_name_start [string wordstart $sig end]
+    puts -nonewline $outf "[string range $sig $sig_name_start end];"
+}
+puts $outf ""
+
+for {set step 0} 1 {incr step} {
+    # log the value of each signal
+    foreach sig $log_signals {
+        puts -nonewline $outf "[examine -noshowbase $sig];"
+    }
+    puts $outf ""
+    # advance by 1 clock cycle
+    run 10 ns
+
+    # check if the abort condition has been met
+    if {[examine -noshowbase $complete_signal] == "1"} {
+        puts "exit condition met after $step clock cycles"
+        break
+    }
+}
+
+exit -f
