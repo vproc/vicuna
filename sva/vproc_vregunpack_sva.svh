1dae87bfe62274d184ddc5f1dde3b2a983671eb6
Michael Platzer <michael.platzer@hotmail.com>
2022-05-16 09:27:18 +0200

    Assert that read ports are only read by one operand at a time
    Multiple operands being sourced from the same read port must never be
    fetched simultaneously.

--- sva/vproc_vregunpack_sva.svh
+++ sva/vproc_vregunpack_sva.svh
@@ -2,6 +2,23 @@
 // Licensed under the Solderpad Hardware License v2.1, see LICENSE.txt for details
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
+    // Assert that there are never two simultaneous reads from the same vreg read port
+    generate
+        for (genvar i = 0; i < OP_CNT; i++) begin
+            for (genvar j = i + 1; j < OP_CNT; j++) begin
+                if (OP_SRC[i] == OP_SRC[j]) begin
+                    assert property (
+                        @(posedge clk_i)
+                        op_addressing[i] |-> ~op_addressing[j]
+                    ) else begin
+                        $error("simultaneous addressing for operands %d and %d sharing port %d",
+                               i, j, OP_SRC[i]);
+                    end
+                end
+            end
+        end
+    endgenerate
+
     // Assert that the requested vreg read addresses correspond to the registers being fetched
     generate
         for (genvar i = 0; i < OP_CNT; i++) begin
