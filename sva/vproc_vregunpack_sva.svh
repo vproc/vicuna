27fb91058247d8f7680733ff6b1fb3bd84ed7468
Michael Platzer <michael.platzer@hotmail.com>
2022-05-18 07:30:15 +0200

    Adapt vregunpack assertions to new parameters
    Change the assertions for the vreg unpack module to be compatible with
    the new parameters introduced in 6214045.

--- sva/vproc_vregunpack_sva.svh
+++ sva/vproc_vregunpack_sva.svh
@@ -22,7 +22,7 @@
     // Assert that the requested vreg read addresses correspond to the registers being fetched
     generate
         for (genvar i = 0; i < OP_CNT; i++) begin
-            if (~VPORT_ADDR_ZERO[OP_SRC[i]]) begin
+            if (OP_SRC[i] < VPORT_CNT) begin
                 assert property (
                     @(posedge clk_i)
                     op_addressing[i] |-> (vreg_rd_addr_o[OP_SRC[i]] == op_vreg_addr[i])
@@ -36,7 +36,7 @@
     // Assert that a vreg is still in the pending reads while being fetched
     generate
         for (genvar i = 0; i < OP_CNT; i++) begin
-            if (~OP_DYN_ADDR[i]) begin
+            if ((OP_SRC[i] < VPORT_CNT) & ~OP_DYN_ADDR[i]) begin
                 assert property (
                     @(posedge clk_i)
                     op_addressing[i] |-> pending_vreg_reads_o[vreg_rd_addr_o[OP_SRC[i]]]
