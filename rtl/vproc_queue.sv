7a241d6da138f64f50bf9f05ce5aede3b0df374a
Michael Platzer <michael.platzer@hotmail.com>
2022-06-06 16:52:06 +0200

    Restructure queue module and add flow-through option
    Structure the queue module as RTL logic with separate flip-flop and
    combinatorial assignment logic.  Add an option that allows flow-through
    of input data directly to output if the queue is empty.

--- rtl/vproc_queue.sv
+++ rtl/vproc_queue.sv
@@ -4,8 +4,9 @@
 
 
 module vproc_queue #(
-        parameter int unsigned   WIDTH = 8,
-        parameter int unsigned   DEPTH = 8
+        parameter int unsigned   WIDTH = 8,   // width of the queue (i.e., width of data elements)
+        parameter int unsigned   DEPTH = 8,   // depth of the queue (i.e., max number of elements)
+        parameter bit            FLOW  = 1'b0 // enable flow-through (pass data through if empty)
     )(
         input  logic             clk_i,
         input  logic             async_rst_ni,
@@ -24,53 +25,84 @@ module vproc_queue #(
     );
 
     logic [WIDTH-1:0]         data[DEPTH];
-    logic [$clog2(DEPTH)-1:0] rd_pos, wr_pos; // read and write positions
-    logic                     last_wr;        // true if last access was write
-
+    logic [$clog2(DEPTH)-1:0] rd_pos_q,  rd_pos_d;  // read position
+    logic [$clog2(DEPTH)-1:0] wr_pos_q,  wr_pos_d;  // write position
+    logic                     last_wr_q, last_wr_d; // true if last access was write
     always_ff @(posedge clk_i or negedge async_rst_ni) begin
         if (~async_rst_ni) begin
-            rd_pos  <= '0;
-            wr_pos  <= '0;
-            last_wr <= '0;
+            rd_pos_q  <= '0;
+            wr_pos_q  <= '0;
+            last_wr_q <= '0;
         end
         else if (~sync_rst_ni) begin
-            rd_pos  <= '0;
-            wr_pos  <= '0;
-            last_wr <= '0;
+            rd_pos_q  <= '0;
+            wr_pos_q  <= '0;
+            last_wr_q <= '0;
         end else begin
-            if (enq_ready_o & enq_valid_i) begin
-                wr_pos       <= (wr_pos == $clog2(DEPTH)'(DEPTH-1)) ? '0 : wr_pos + 1;
-                last_wr      <= 1'b1;
-            end
-            if (deq_ready_i & deq_valid_o) begin
-                rd_pos       <= (rd_pos == $clog2(DEPTH)'(DEPTH-1)) ? '0 : rd_pos + 1;
-                last_wr      <= 1'b0;
-            end
-        end
-    end
-    always_ff @(posedge clk_i) begin
-        if (enq_ready_o & enq_valid_i) begin
-            data[wr_pos] <= enq_data_i;
+            rd_pos_q  <= rd_pos_d;
+            wr_pos_q  <= wr_pos_d;
+            last_wr_q <= last_wr_d;
         end
     end
 
-    assign enq_ready_o = (rd_pos != wr_pos) | ~last_wr;
-    assign deq_valid_o = (rd_pos != wr_pos) |  last_wr;
-    assign deq_data_o  = data[rd_pos];
+    logic empty, full;
+    assign empty = (rd_pos_q == wr_pos_q) & ~last_wr_q;
+    assign full  = (rd_pos_q == wr_pos_q) &  last_wr_q;
+
+    always_ff @(posedge clk_i) begin
+        if (~full) begin
+            data[wr_pos_q] <= enq_data_i;
+        end
+    end
+
+    logic push, pop;
+    always_comb begin
+        push        = 1'b0;
+        pop         = 1'b0;
+        enq_ready_o = 1'b0;
+        deq_valid_o = 1'b0;
+        deq_data_o  = data[rd_pos_q];
+        if (~full) begin
+            push        = enq_valid_i & (~FLOW | ~empty | ~deq_ready_i);
+            enq_ready_o = 1'b1;
+        end
+        if (~empty) begin
+            pop         = deq_ready_i;
+            deq_valid_o = 1'b1;
+        end
+        else if (FLOW) begin
+            deq_valid_o = enq_valid_i;
+            deq_data_o  = enq_data_i;
+        end
+    end
+
+    always_comb begin
+        rd_pos_d  = rd_pos_q;
+        wr_pos_d  = wr_pos_q;
+        last_wr_d = last_wr_q;
+        if (push) begin
+            wr_pos_d  = (wr_pos_q == $clog2(DEPTH)'(DEPTH-1)) ? '0 : wr_pos_q + $clog2(DEPTH)'(1);
+            last_wr_d = 1'b1;
+        end
+        if (pop) begin
+            rd_pos_d  = (rd_pos_q == $clog2(DEPTH)'(DEPTH-1)) ? '0 : rd_pos_q + $clog2(DEPTH)'(1);
+            last_wr_d = 1'b0;
+        end
+    end
 
     // Mask of valid entries
     logic [DEPTH-1:0] entry_valid;
     always_comb begin
         entry_valid = '0;
-        if (rd_pos == wr_pos) begin
+        if (rd_pos_q == wr_pos_q) begin
             // either all or no entries are valid, depending on last action
-            entry_valid = {DEPTH{last_wr}};
+            entry_valid = {DEPTH{last_wr_q}};
         end else begin
             for (int i = 0; i < DEPTH; i++) begin
-                if (rd_pos < wr_pos) begin
-                    entry_valid[i] = (i >= rd_pos) & (i < wr_pos);
+                if (rd_pos_q < wr_pos_q) begin
+                    entry_valid[i] = (i >= rd_pos_q) & (i < wr_pos_q);
                 end else begin
-                    entry_valid[i] = (i >= rd_pos) | (i < wr_pos);
+                    entry_valid[i] = (i >= rd_pos_q) | (i < wr_pos_q);
                 end
             end
         end
